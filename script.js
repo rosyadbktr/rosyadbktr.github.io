83ed39bdeb8caffc414f8263b8596dd8c1ccb258
supeRosyad <rosyadandro@gmail.com>
2023-05-29 21:25:44 +0700

    memperbaiki tampilan mobile

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const link = document.querySelector('.link');
 const shareLink = document.querySelector('.share-link')
 link.addEventListener('mouseover', () => shareLink.classList.remove('hidden'))
 link.addEventListener('mouseleave', () => shareLink.classList.add('hidden'))
+
+const documentHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', documentHeight);
